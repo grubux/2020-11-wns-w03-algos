e13777e5f39a4efb0c85fb057a8852d17718f30b
Lorris Lehuede <grubux@pop-os.localdomain>
2021-09-21 11:29:44 +0200

    A1 + A2 + A3 done

--- src/challenges/A1/answer.ts
+++ src/challenges/A1/answer.ts
@@ -1,21 +1,32 @@
 /**
  * In this challenge, you must sort students by their age (younger first). If some of them have
  * the same age, then you should sort them alphabetically (A to Z)
- * 
+ *
  * @param students Unsorted list of students
  * @returns Sorted list of students
  */
 
 // ↓ uncomment bellow lines and add your response!
-/*
+
+const compareStudents = (a: Student, b: Student): number => {
+  if (a.age < b.age) {
+    return -1;
+  } else if (a.age > b.age) {
+    return 1;
+  } else if (a.age === b.age) {
+    return a.name.localeCompare(b.name);
+  } else {
+    return 0;
+  }
+};
+
 export default function ({ students }: { students: Student[] }): Student[] {
-    return [];
+  return students.sort(compareStudents);
 }
-*/
 
 // used interfaces, do not touch
 export interface Student {
-    name: string;
-    age: number;
-    skills: string[];
+  name: string;
+  age: number;
+  skills: string[];
 }
--- src/challenges/A3/answer.ts
+++ src/challenges/A3/answer.ts
@@ -1,28 +1,51 @@
 /**
-* In this challenge, you should sort messages by their sentAt property (oldest first) and 
-* modify them by adding an "unread" property
-* (boolean meaning that the current user did not read this message) based on the lastActivityDatetime
-* input. 
-*
-* @param lastActivityDatetime String representing an ISO8601 datetime. Represent the last time the user checked his messages
-* @param messages List of messages, unsorted and without unread property
-* @returns Sorted list of messages with the unread information
-*/
+ * In this challenge, you should sort messages by their sentAt property (oldest first) and
+ * modify them by adding an "unread" property
+ * (boolean meaning that the current user did not read this message) based on the lastActivityDatetime
+ * input.
+ *
+ * @param lastActivityDatetime String representing an ISO8601 datetime. Represent the last time the user checked his messages
+ * @param messages List of messages, unsorted and without unread property
+ * @returns Sorted list of messages with the unread information
+ */
 
-// ↓ uncomment bellow lines and add your response!
-/*
-export default function ({ lastActivityDatetime, messages } : { lastActivityDatetime: string, messages: Message[] }): MessageWithUnread[] {
-    return [];
+// ↓ uncomment bellow lines and add your response
+
+export default function ({
+  lastActivityDatetime,
+  messages,
+}: {
+  lastActivityDatetime: string;
+  messages: Message[];
+}): MessageWithUnread[] {
+  return messages
+    .map(({ ...user }) => {
+      const dateActivity: Date = new Date(lastActivityDatetime);
+      const dateSentAt: Date = new Date(user.sentAt);
+      const dateSentAtString = dateSentAt.getTime();
+      const dateActivityString = dateActivity.getTime();
+
+      let unreadRaw: boolean;
+      if (dateActivityString < dateSentAtString) {
+        unreadRaw = true;
+      } else if (dateActivityString > dateSentAtString) {
+        unreadRaw = false;
+      } else {
+        unreadRaw = false;
+      }
+
+      return { ...user, unread: unreadRaw };
+    })
+    .sort((a, b) => Date.parse(a.sentAt) - Date.parse(b.sentAt));
 }
-*/
 
 // used interfaces, do not touch
 export interface Message {
-    author: string;
-    sentAt: string;
-    message: string;
+  author: string;
+  sentAt: string;
+  message: string;
 }
 
 export interface MessageWithUnread extends Message {
-    unread: boolean;
+  unread: boolean;
 }
